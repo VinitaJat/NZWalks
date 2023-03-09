6b1cd00a5a30504c3a9a838b3ac2d481b01b7329
Vinita87jat <vinita.jat@imriel.com>
2023-03-09 15:37:31 +0530

    Complete DB connection

---
+++ NZWalks/NZWalks.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksDbContext : DbContext
+    {
+        public NZWalksDbContext(DbContextOptions<NZWalksDbContext> options): base(options)
+        {
+
+        }
+    public DbSet<Region> Regions { get; set; }
+    public DbSet<Walkcs> Walkcs { get; set; }
+    public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
---
+++ NZWalks/NZWalks.API/Models/Domain/Region.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime;
+
+namespace NZWalks.API.Models.Domain
+{
+    public class Region
+    {
+        public Guid Id { get; set; }  
+        public String Code { get; set; }
+        public string Name { get; set; }
+        public double Area { get; set; }
+        public double Lat { get; set; }
+        public double Long { get; set; }
+        public double Population { get; set; }
+
+        // Navigation Property
+    public IEnumerable<Walkcs> Walks { get; set; }
+
+
+    }
+}
